a430307eee9830b224aff92038e83cc8fa054ce5
rogerbf <roger@bergfalk.nu>
2016-11-30 21:49:59 +0100

    move to if/else if

--- source/library/severity.js
+++ source/library/severity.js
@@ -1,17 +1,23 @@
-const levels = [
-  { name: `debug`, regex: /\[debug]/g },
-  { name: `info`, regex: /\[info]/g },
-  { name: `notice`, regex: /\[notice]/g },
-  { name: `warn`, regex: /\[warn]/g },
-  { name: `err`, regex: /\[err]/g }
-]
-
 export default payload => {
-  return levels.reduce((value, severity) => {
-    if (severity.regex.test(payload.data)) {
-      return { ...payload, severity: severity.name }
-    } else {
-      return value
-    }
-  })
+  let severity
+
+  if (payload.data.indexOf(`[debug]`) !== -1) {
+    severity = `debug`
+  } else if (payload.data.indexOf(`[info]`) !== -1) {
+    severity = `info`
+  } else if (payload.data.indexOf(`[notice]`) !== -1) {
+    severity = `notice`
+  } else if (payload.data.indexOf(`[warn]`) !== -1) {
+    severity = `warn`
+  } else if (payload.data.indexOf(`[err]`) !== -1) {
+    severity = `err`
+  } else {
+    severity = ``
+  }
+
+  if (severity.length > 0) {
+    return { ...payload, severity }
+  } else {
+    return { ...payload }
+  }
 }
--- source/test/severity.test.js
+++ source/test/severity.test.js
@@ -6,10 +6,51 @@ test(`is a function`, assert => {
   assert.end()
 })
 
+test(`return payload unmodified payload`, assert => {
+  const payload = { data: `hello there` }
+  const expected = payload
+  assert.deepEqual(severity(payload), expected)
+  assert.end()
+})
+
+test(`debug`, assert => {
+  const payload = { data: `hello [debug]` }
+  const expected = { ...payload, severity: `debug` }
+  assert.deepEqual(severity(payload), expected)
+  assert.end()
+})
+
+test(`info`, assert => {
+  const payload = { data: `hello [info]` }
+  const expected = { ...payload, severity: `info` }
+  assert.deepEqual(severity(payload), expected)
+  assert.end()
+})
+
+test(`notice`, assert => {
+  const payload = { data: `hello [notice]` }
+  const expected = { ...payload, severity: `notice` }
+  assert.deepEqual(severity(payload), expected)
+  assert.end()
+})
+
+test(`warn`, assert => {
+  const payload = { data: `hello [warn]` }
+  const expected = { ...payload, severity: `warn` }
+  assert.deepEqual(severity(payload), expected)
+  assert.end()
+})
+
+test(`err`, assert => {
+  const payload = { data: `hello [err]` }
+  const expected = { ...payload, severity: `err` }
+  assert.deepEqual(severity(payload), expected)
+  assert.end()
+})
+
 test(`expected output`, assert => {
   const payload = { data: `Nov 23 11:27:43.438 [notice] Configuration file "/usr/local/etc/tor/torrc" not present, using reasonable defaults.` }
   const expected = { ...payload, severity: `notice` }
-  const actual = severity(payload)
-  assert.deepEqual(actual, expected)
+  assert.deepEqual(severity(payload), expected)
   assert.end()
 })

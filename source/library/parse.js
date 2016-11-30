ab8147b4a928ca167564f9e3ea890fdfb64ce471
rogerbf <roger@bergfalk.nu>
2016-11-30 21:51:59 +0100

    use parseLine

--- source/library/parse.js
+++ source/library/parse.js
@@ -1,9 +1,10 @@
 import { Transform } from 'stream'
+import parseLine from './parseLine'
 
 export default () => {
   return new Transform({
     transform (chunk, encoding, next) {
-      this.push(chunk.toString())
+      this.push(parseLine(chunk.toString()))
       next()
     },
     objectMode: true
--- source/test/parse.test.js
+++ source/test/parse.test.js
@@ -2,29 +2,27 @@ import test from 'tape'
 import parse from '../library/parse'
 import { Readable } from 'stream'
 
-// const createTestStream = (data, objectMode = false) => {
-//   return Object.assign(
-//     new Readable({
-//       read () {
-//         if (this.data.length > 0) {
-//           this.push(this.data.shift())
-//         } else {
-//           this.push(null)
-//         }
-//       },
-//       objectMode
-//     }),
-//     { data: [ ...data ] }
-//   )
-// }
-//
-// const testData = {
-//   A: [
-//     `Nov 23 11:27:43.438 [notice] Configuration file "/usr/local/etc/tor/torrc" not present, using reasonable defaults.`,
-//     `Nov 23 11:27:43.446 [warn] Warning from libevent: kq_init: detected broken kqueue; not using.: Invalid argument`,
-//     `Nov 23 11:27:43.446 [notice] Opening Socks listener on 127.0.0.1:9050`
-//   ]
-// }
+const createTestStream = (data, objectMode = false) => {
+  return Object.assign(
+    new Readable({
+      read () {
+        if (this.data.length > 0) {
+          this.push(this.data.shift())
+        } else {
+          this.push(null)
+        }
+      },
+      objectMode
+    }),
+    { data: [ ...data ] }
+  )
+}
+
+const testData = [
+  `Nov 23 11:27:43.438 [notice] Configuration file "/usr/local/etc/tor/torrc" not present, using reasonable defaults.`,
+  `Nov 23 11:27:43.446 [warn] Warning from libevent: kq_init: detected broken kqueue; not using.: Invalid argument`,
+  `Nov 23 11:27:43.446 [notice] Opening Socks listener on 127.0.0.1:9050`
+]
 
 test(`parser`, assert => {
   assert.equal(typeof (parse), `function`)
@@ -32,42 +30,28 @@ test(`parser`, assert => {
   assert.end()
 })
 
-// test(`expected output`, assert => {
-//   const source = createTestStream(testData.A)
-//   const parser = parse()
-//
-//   const expected = {
-//     eventCount: 3,
-//     data: [
-//       {
-//         severity: `notice`,
-//         raw: `Nov 23 11:27:43.438 [notice] Configuration file "/usr/local/etc/tor/torrc" not present, using reasonable defaults.`
-//       },
-//       {
-//         severity: `warn`,
-//         raw: `Nov 23 11:27:43.446 [warn] Warning from libevent: kq_init: detected broken kqueue; not using.: Invalid argument`
-//       },
-//       {
-//         severity: `notice`,
-//         raw: `Nov 23 11:27:43.446 [notice] Opening Socks listener on 127.0.0.1:9050`
-//       }
-//     ]
-//   }
-//
-//   const actual = {
-//     eventCount: 0,
-//     data: []
-//   }
-//
-//   parser.on(`data`, data => {
-//     actual.data.push(data)
-//     actual.eventCount = actual.eventCount + 1
-//   })
-//
-//   parser.on(`end`, () => {
-//     assert.deepEqual(actual, expected)
-//     assert.end()
-//   })
-//
-//   source.pipe(parser)
-// })
+test(`expected output`, assert => {
+  const source = createTestStream(testData)
+  const parser = parse()
+
+  const expected = {
+    eventCount: 3
+  }
+
+  const actual = {
+    eventCount: 0,
+    data: []
+  }
+
+  parser.on(`data`, data => {
+    actual.data.push(data)
+    actual.eventCount = actual.eventCount + 1
+  })
+
+  parser.on(`end`, () => {
+    assert.deepEqual(actual.eventCount, expected.eventCount)
+    assert.end()
+  })
+
+  source.pipe(parser)
+})

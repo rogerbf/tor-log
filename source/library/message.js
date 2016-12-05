ecc858620329db7a2976d5b3f8a0a201eedd8886
rogerbf <roger@bergfalk.nu>
2016-12-05 23:03:24 +0100

    switched to ternary operation

--- source/library/message.js
+++ source/library/message.js
@@ -1,8 +1,8 @@
 export default payload => {
   const messageStart = payload.data.indexOf(`]`)
-  if (messageStart !== -1) {
-    return { ...payload, message: payload.data.slice(messageStart + 1).trim() }
-  } else {
-    return { ...payload }
-  }
+  return (
+    messageStart !== -1
+    ? { ...payload, message: payload.data.slice(messageStart + 1).trim() }
+    : { ...payload }
+  )
 }

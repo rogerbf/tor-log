879a31e2973fb02b8bed3ee9155eaa091ae9c4cc
rogerbf <roger@bergfalk.nu>
2016-12-06 00:24:05 +0100

    merged toObject into parseLine

--- source/library/parseLine.js
+++ source/library/parseLine.js
@@ -1,9 +1,8 @@
-import toObject from './toObject'
 import timestamp from './timestamp'
 import severity from './severity'
 import message from './message'
 
-export default (line) => (
+export default line => (
   [ timestamp, severity, message ]
-  .reduce((status, extractor) => extractor(status), toObject(line))
+  .reduce((status, extractor) => extractor(status), { data: line })
 )

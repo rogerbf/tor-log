1b7abad842980b5ae97542467ebf570eb62f8148
rogerbf <roger@bergfalk.nu>
2016-12-05 23:14:48 +0100

    refactoring, remove else if infestation

--- source/library/severity.js
+++ source/library/severity.js
@@ -1,23 +1,14 @@
 export default payload => {
-  let severity
+  const severity = (
+    [ `debug`, `info`, `notice`, `warn`, `err` ]
+    .reduce((acc, level) => {
+      return payload.data.indexOf(level) !== -1 ? level : acc
+    }, undefined)
+  )
 
-  if (payload.data.indexOf(`[debug]`) !== -1) {
-    severity = `debug`
-  } else if (payload.data.indexOf(`[info]`) !== -1) {
-    severity = `info`
-  } else if (payload.data.indexOf(`[notice]`) !== -1) {
-    severity = `notice`
-  } else if (payload.data.indexOf(`[warn]`) !== -1) {
-    severity = `warn`
-  } else if (payload.data.indexOf(`[err]`) !== -1) {
-    severity = `err`
-  } else {
-    severity = ``
-  }
-
-  if (severity.length > 0) {
-    return { ...payload, severity }
-  } else {
-    return { ...payload }
-  }
+  return (
+    severity
+    ? { ...payload, severity }
+    : { ...payload }
+  )
 }

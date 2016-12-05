cc02bb95b3efcc41292453bc6c0e5343d4dff10e
rogerbf <roger@bergfalk.nu>
2016-12-05 23:06:02 +0100

    refactoring

--- source/library/parseLine.js
+++ source/library/parseLine.js
@@ -3,12 +3,8 @@ import timestamp from './timestamp'
 import severity from './severity'
 import message from './message'
 
-const extractors = [
-  timestamp, severity, message
-]
-
-export default (line) => {
-  return extractors.reduce((status, extractor) => {
+export default (line) => (
+  [ timestamp, severity, message ].reduce((status, extractor) => {
     return extractor(status)
   }, toObject(line))
-}
+)

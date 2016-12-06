750bf50c9badaaeebc6ce0f6d1520f7d656f3ef7
rogerbf <roger@bergfalk.nu>
2016-12-06 16:39:13 +0100

    rename parseTransform to parser

--- source/index.js
+++ source/index.js
@@ -1,11 +1,11 @@
-import parseTransform from './library/parseTransform'
+import parser from './library/parseTransform'
 import lumberman from 'lumberman'
 import split from 'buffer-split-transform'
 
 export default readable => {
   return lumberman({
     source: readable.stdout ? readable.stdout : readable,
-    transform: [ split(), parseTransform() ],
+    transform: [ split(), parser() ],
     emit: [
       { eventName: `debug`, filter: data => data.severity === `debug` },
       { eventName: `info`, filter: data => data.severity === `info` },
